94f57f4227be859870c3a756b57b483b0e95b5d6
dannielhso <dannielha@naver.com>
2025-03-16 16:08:09 +0900

    chore : 생성한 페이지 및 헤더 의존성 주입으로 연결 (#7)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouteReuseStrategy } from '@angular/router';
-
+import { RouteReuseStrategy, RouterModule } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { LayoutModule } from './layouts/layouts.module';
+import { HeaderComponent } from './layouts/header/header.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    LayoutModule,
+    HeaderComponent
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })

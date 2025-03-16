13b8a5543040912f433fbb956b48d1e45d6f43f4
dannielhso <dannielha@naver.com>
2025-03-16 16:04:21 +0900

    feat : 상단 네비바 기능 구현 및 의존성 주입 (#7)

---
+++ src/app/layouts/header/header.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { IonicModule } from '@ionic/angular';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
+  standalone: true,
+  imports: [
+    IonicModule,
+    RouterModule,
+  ]
+})
+export class HeaderComponent {}

8fc0c356a1bc2b14a4ab7801e06f729f5f068dbe
kdk1248 <alice3976@g.hongik.ac.kr>
2025-03-18 14:45:23 +0900

    feat: 유물 데이터 구조체 정의 (📜 Heritage #12)

---
+++ src/app/models/heritage/heritage-getheritagesbyid-response-data.interface.ts
@@ -0,0 +1,8 @@
+export interface GetHeritagesByIdResponseData {
+  "heritageId": number;
+  "heritageName": string;
+  "heritageDescription": string;
+  "heritageLocation": string;
+  "heritageLatitude": number;
+  "heritageLongitude": number;
+}

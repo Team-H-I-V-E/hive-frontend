ae345c5f636e598131ec4f938b0fe6ef95ae2a1d
kdk1248 <alice3976@g.hongik.ac.kr>
2025-03-18 14:46:06 +0900

    feat: 유물 관련 컴포넌트 및 서비스 관리 모듈 구현, 라우팅 연결 (📜 Heritage #12)

--- src/app/pages/heritage/heritage.module.ts
+++ src/app/pages/heritage/heritage.module.ts
@@ -1,12 +1,22 @@
-import { NgModule } from "@angular/core";
-import { HeritageComponent } from "./heritage.component";
-import { HeritageRoutingModule } from "./heritage-routing.module";
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeritageListComponent } from './heritage-list/heritage-list.component';
+import { HeritageDetailComponent } from './heritage-detail/heritage-detail.component';
+import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
+import { HeritageRoutingModule } from './heritage-routing.module';
 
 @NgModule({
-    imports: [
-        HeritageComponent,
-        HeritageRoutingModule
+    declarations: [
+        HeritageListComponent,
+        HeritageDetailComponent,
     ],
-    exports: [HeritageComponent]
+    imports: [
+        CommonModule,
+        IonicModule,
+        FormsModule,
+        HeritageRoutingModule,
+
+    ]
 })
-export class HeritageComponentModule {}
+export class HeritageComponentModule { }

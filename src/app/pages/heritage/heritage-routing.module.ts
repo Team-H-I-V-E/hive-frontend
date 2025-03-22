37dae112a9b90e3dbb70814418160abd5bdc9945
kdk1248 <alice3976@g.hongik.ac.kr>
2025-03-22 17:12:04 +0900

    refactor: redirectTo 로 인한 자동 경로 변경 수정 및 유물목록 페이지 경로 추가 (📜 Heritage-KaKaMap #19)

--- src/app/pages/heritage/heritage-routing.module.ts
+++ src/app/pages/heritage/heritage-routing.module.ts
@@ -1,25 +1,17 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
-import { HeritageComponent } from "./heritage.component";
 import { HeritageDetailComponent } from "./heritage-detail/heritage-detail.component";
 import { HeritageListComponent } from "./heritage-list/heritage-list.component";
 
 const routes: Routes = [
     {
-        path: '',
-        redirectTo: 'heritageList',  
-        pathMatch: 'full'
+        path: 'heritageList',
+        component: HeritageListComponent
     },
     {
         path: 'heritagedetail/:id',
         component: HeritageDetailComponent
-    },
-    {
-        path: 'heritageList',
-        component: HeritageListComponent
-    },
-
-    
+    },   
 ];
 
 @NgModule({

4b11ee053367c76598f438a1a1ec232e66a6ea22
dannielhso <dannielha@naver.com>
2025-04-05 11:41:24 +0900

    refactor : article 엔터티 수정 (#29)

--- src/app/models/article/article.model.ts
+++ src/app/models/article/article.model.ts
@@ -1,9 +1,11 @@
+import { ArticleImages } from "./articleImage.model";
+
 export interface Article {
-    articleId: number;
-    userID: number;
-    articleTitle: string;
-    articleContents: string;
-    articleCreatedAt: Date;
-    articleUpdatedAt?: Date;
-    articleImages: string[];
-  }  
+    "articleId": number;
+    "userId": number;
+    "articleTitle": string;
+    "articleContents": string;
+    "articleCreatedAt": Date;
+    "articleUpdatedAt?": Date;
+    "articleImages": ArticleImages[];
+  }

2e6f50e89b34d1466ccc9e5510a6d091b51642d6
Kwahk <your_email@example.com>
2025-04-16 15:24:14 +0900

    feat : arexplore service 수정 및 stamp model 추가 (#39)

--- src/app/services/arexplore/arexplore.service.ts
+++ src/app/services/arexplore/arexplore.service.ts
@@ -1,16 +1,21 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Stamp } from 'src/app/models/arexplore/stamp.model';
 import { environment } from 'src/environment/environment';
 
-@Injectable({ providedIn: 'root' })
+@Injectable({ 
+  providedIn: 'root' 
+})
 export class ArExploreService {
     private baseUrl = `${environment.apiBaseUrl}/api/arexperience`;
+    // private readonly API_URL = 'http://localhost:3000/api/arexperience';
 
   constructor(private http: HttpClient) {}
 
-  // 유저가 아직 못 받은 스탬프 목록 가져오기
-  getUnacquiredStamps(userId: number) {
-    return this.http.get(`${this.baseUrl}/${userId}/unacquired-stamps`);
+  // 사용자가 획득하지 않은 스탬프 목록 가져오기
+  getUnacquiredStamps(userId: number): Observable<Stamp[]> {
+    return this.http.get<Stamp[]>(`${this.baseUrl}/${userId}/unacquired-stamps`);
   }
 
   // 스탬프 획득 요청

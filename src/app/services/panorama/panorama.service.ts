f85c91a817c27ae5525e96262364f10d93a17d09
dannielhso <dannielha@naver.com>
2025-05-14 13:18:12 +0900

    fix : DTO 수정으로 인한 수정 (#54)

--- src/app/services/panorama/panorama.service.ts
+++ src/app/services/panorama/panorama.service.ts
@@ -1,8 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { GetPanoramaResponseData } from 'src/app/models/panorama/panorama-getpanorama-response-data.interface';
-import { GetPanoramaByIdResponseData } from 'src/app/models/panorama/panorama-getpanoramabyid-response-data.interface';
+import { GetPanoramaByIdResponseData, GetPanoramaResponseData } from 'src/app/models/panorama/panorama-response-data.interface';
 
 @Injectable({
   providedIn: 'root',

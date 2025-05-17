10c100614efd56e64dd73969f3d128c8a5b4bd75
Kwahk <your_email@example.com>
2025-05-17 16:23:08 +0900

    feat : CollectionStamp 스탬프 상세 모달 로직 작성 (#63)

--- src/app/pages/collectionStamp/collection-stamp.component.ts
+++ src/app/pages/collectionStamp/collection-stamp.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient }        from '@angular/common/http';
-import { StampList } from 'src/app/models/collectionStamp/stamp-list.interface';
+import { CollectionStampService } from '../../services/collectionStamp/collectionStamp.service';
+import { StampList } from '../../models/collectionStamp/stamp-list.interface';
+import { StampDetail } from '../../models/collectionStamp/stamp-detail.interface';
 
 @Component({
   selector: 'app-collection-stamp',
@@ -9,26 +10,24 @@ import { StampList } from 'src/app/models/collectionStamp/stamp-list.interface';
   standalone: false,
 })
 export class CollectionStampComponent implements OnInit {
-  private readonly API_URL = '/api/mystamps';
-
   stamps: StampList[] = [];
+  detail: StampDetail | null = null;
+  showDetail = false;
 
-  constructor(private http: HttpClient) {}
+  constructor(private svc: CollectionStampService) {}
 
   ngOnInit() {
-    this.loadStamps();
+    this.svc.getMyStamps().subscribe(data => this.stamps = data);
   }
 
-  private loadStamps() {
-    console.log('📨 내 스탬프 목록 요청');
-    this.http.get<StampList[]>(this.API_URL).subscribe({
-      next: (data) => {
-        this.stamps = data;
-        console.log('📦 받은 스탬프 목록:', this.stamps);
-      },
-      error: (err) => {
-        console.error('❌ 스탬프 조회 실패', err);
-      },
+  onImageClick(s: StampList) {
+    this.svc.getStampDetail(s.stampID).subscribe(d => {
+      this.detail = { ...d, stampImage: s.stampImage };
+      this.showDetail = true;
     });
+  }
+
+  closeDetail() {
+    this.showDetail = false;
   }
 }

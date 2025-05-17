6ec4bb364dcc6603a61e8513d427c38ceae9c9d4
Kwahk <your_email@example.com>
2025-05-17 14:15:52 +0900

    feat : 스탬프 목록 조회 로직 구현 (#63)

---
+++ src/app/services/collectionStamp/collectionStamp.service.ts
@@ -0,0 +1,16 @@
+import { Injectable }    from '@angular/core';
+import { HttpClient }    from '@angular/common/http';
+import { Observable }    from 'rxjs';
+import { StampList } from 'src/app/models/collectionStamp/stamp-list.interface';
+
+@Injectable({ providedIn: 'root' })
+export class CollectionStampService {
+  private readonly API = '/api/mystamps';
+
+  constructor(private http: HttpClient) {}
+
+  // 내 스탬프 목록 가져오기
+  getMyStamps(): Observable<StampList[]> {
+    return this.http.get<StampList[]>(this.API);
+  }
+}

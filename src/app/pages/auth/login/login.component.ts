b1aa4d3497262c27c88ef590dba96b02803cee4e
SMK <coz0703@naver.com>
2025-05-18 14:56:08 +0900

    feat: 로그인 typescript 로직 작성

---
+++ src/app/pages/auth/login/login.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent {
+  loginForm: FormGroup;
+  showPassword: boolean = false;
+
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+  }
+
+  togglePasswordVisibility(): void {
+    this.showPassword = !this.showPassword;
+  }
+
+  onSubmit(): void {
+    if (this.loginForm.valid) {
+      const credentials = this.loginForm.value;
+      // TODO: 로그인 API 호출
+      console.log('로그인 정보:', credentials);
+    }
+  }
+}

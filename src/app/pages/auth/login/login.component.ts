469254029e5e5a338122686f393c4eb2544e96a9
dannielhso <dannielha@naver.com>
2025-05-19 12:08:17 +0900

    fix : Auth 회원가입 이동 오류 및 로고 오류 수정 (#77)

--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -4,7 +4,8 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
+  standalone: false,
 })
 export class LoginComponent {
   loginForm: FormGroup;

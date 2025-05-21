a212545e31f80db080e178890dd5c0e6d27ab953
Kwahk <your_email@example.com>
2025-05-22 00:29:36 +0900

    feat : 문화유산 데이터 가져오는 로직 구현 (#78)

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { CulturalAsset } from 'src/app/models/home/home-response-data.interface';
 
 @Component({
   selector: 'app-home',
@@ -6,9 +8,26 @@ import { Component } from '@angular/core';
   styleUrls: ['home.component.scss'],
   standalone: false,
 })
-export class HomeComponent {
-  constructor() {}
+export class HomeComponent implements OnInit {
+  assets: CulturalAsset[] = [];
+  private readonly API_URL = 'http://localhost:3000';
+
+  constructor(private http: HttpClient) {}
+
   ngOnInit(): void {
     console.log('🏠 HomeComponent loaded!');
+    this.loadCulturalAssets();
+  }
+
+  loadCulturalAssets(): void {
+    this.http.get<CulturalAsset[]>(`${this.API_URL}/home`).subscribe({
+      next: (data) => {
+        this.assets = data;
+        console.log('📦 문화유산 데이터:', this.assets);
+      },
+      error: (err) => {
+        console.error('❌ 데이터 로드 실패:', err);
+      }
+    });
   }
 }

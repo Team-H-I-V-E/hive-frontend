d2578c141b887776acb6ed545bbf7efa3d47b9c5
SMK <coz0703@naver.com>
2025-05-21 17:28:16 +0900

    feat: 로그인 기능 구현을 위한 Router 및 AuthService 모듈 추가

--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -10,10 +12,14 @@ export class LoginComponent {
   loginForm: FormGroup;
   showPassword: boolean = false;
 
-  constructor(private fb: FormBuilder) {
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {
     this.loginForm = this.fb.group({
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', Validators.required]
+      userEmail: ['', [Validators.required, Validators.email]],
+      userPassword: ['', Validators.required]
     });
   }
 
@@ -21,11 +27,17 @@ export class LoginComponent {
     this.showPassword = !this.showPassword;
   }
 
-  onSubmit(): void {
-    if (this.loginForm.valid) {
-      const credentials = this.loginForm.value;
-      // TODO: 로그인 API 호출
-      console.log('로그인 정보:', credentials);
-    }
+  onLogin(): void {
+    const { userEmail, userPassword } = this.loginForm.value;
+
+    this.authService.signin(userEmail, userPassword).subscribe({
+      next: (res) => {
+        console.log('로그인 성공', res);
+        this.router.navigate(['/']);
+      },
+      error: (err) => {
+        console.error('로그인 실패', err);
+      }
+    });
   }
 }

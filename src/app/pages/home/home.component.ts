c92dff9655e393e64b0e6ea0026bf1a34d02e8fb
Kwahk <your_email@example.com>
2025-05-22 01:45:45 +0900

    feat : startRotatingAssets 5초마다 화면 전환 로직 구현 (#78)

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { CulturalAsset } from 'src/app/models/home/home-response-data.interface';
 
 @Component({
@@ -8,8 +8,12 @@ import { CulturalAsset } from 'src/app/models/home/home-response-data.interface'
   styleUrls: ['home.component.scss'],
   standalone: false,
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
   assets: CulturalAsset[] = [];
+  currentAsset: CulturalAsset | null = null;
+  currentIndex: number = 0;
+  intervalId: any = null;
+
   private readonly API_URL = 'http://localhost:3000';
 
   constructor(private http: HttpClient) {}
@@ -19,15 +23,32 @@ export class HomeComponent implements OnInit {
     this.loadCulturalAssets();
   }
 
+  ngOnDestroy(): void {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
+  }
+
   loadCulturalAssets(): void {
     this.http.get<CulturalAsset[]>(`${this.API_URL}/home`).subscribe({
       next: (data) => {
         this.assets = data;
         console.log('📦 문화유산 데이터:', this.assets);
+        this.startRotatingAssets();
       },
       error: (err) => {
         console.error('❌ 데이터 로드 실패:', err);
       }
     });
+  }
+
+  startRotatingAssets(): void {
+    if (this.assets.length === 0) return;
+    this.currentAsset = this.assets[0];
+
+    this.intervalId = setInterval(() => {
+      this.currentIndex = (this.currentIndex + 1) % this.assets.length;
+      this.currentAsset = this.assets[this.currentIndex];
+    }, 5000);
   }
 }
